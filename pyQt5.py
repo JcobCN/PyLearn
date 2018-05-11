30fc1b1c1b0dae84b4233311536264d82f88a50a
JiangCN <tj@tj>
2018-05-11 17:14:59 +0800

    statusbar

--- pyQt5.py
+++ pyQt5.py
@@ -2,8 +2,10 @@
 import sys
 import win32api
 from PyQt5.QtWidgets import QApplication, QWidget, \
-    QToolTip, QPushButton
+    QToolTip, QPushButton, QMessageBox, QDesktopWidget, \
+    QMainWindow, QAction, QMenu, QStatusBar
 from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
 '''
 #面向过程
@@ -17,27 +19,79 @@ w.setWindowTitle('将图片转换为jpg 1920*1080')
 w.show()
 sys.exit(app.exec_())
 '''
-class Example(QWidget):
+x , y= 0,0
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()#调用父类构造函数
         self.initUI()
 
     def initUI(self):
 
+        global x,y
+
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is a <b>QWidget</b> widget')
 
-        btn = QPushButton('Button', self)
+        btn = QPushButton('Quit', self)
         btn.setToolTip('This is a <b>QPushButton</b> widget')
+        # btn.clicked.connect(QCoreApplication.instance().quit) #clicked btn and exit app
+        btn.clicked.connect(QApplication.instance().exit) #clicked btn and exit app
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
 
-        x, y = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
-        self.setGeometry((x-700)/2, (y-400)/2, 700, 400)
+        exitAct:QAction = QAction(QIcon('46.jpg'), 'Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(QApplication.instance().quit)
+
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('File')
+        fileMenu.addAction(exitAct)
+
+        # subMenu add to FileMenu
+        subMenu = QMenu('Import', self)
+        subAct = QAction('Import file', self)
+        subMenu.addAction(subAct)
+        fileMenu.addMenu(subMenu)
+
+        #checkMenu
+        viewSBar:QAction = QAction('View statusbar', self, checkable=True)
+        viewSBar.setStatusTip('View statusbar')
+        viewSBar.setChecked(True)
+        viewSBar.triggered.connect(self.toggleMenu)
+        fileMenu.addAction(viewSBar)
+
+        # x, y = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
+        # self.setGeometry((x-700)/2, (y-400)/2, 700, 400)
+        self.center()
         self.setFixedSize(700, 400)
         self.setWindowTitle('将图片转换成jpg 1080')
+        self.statusbar = self.statusBar();
+        self.statusbar.showMessage('Ready')
         self.setWindowIcon(QIcon('46.jpg'))
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()#get self RECT
+        cp = QDesktopWidget().availableGeometry().center()#
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    #When Widgets is closing, call this function by automaticlly
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit',
+                                     QMessageBox.Yes|QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def toggleMenu(self, state):
+        print(state)
+        if state:
+            self.statusbar.show()
+        else:
+            self.statusbar.hide()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
